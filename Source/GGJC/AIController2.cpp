23b9b761065dba7ed99f77cf59cee69719c9f1f6
sanjay900 <sanjay.govind9@gmail.com>
2018-01-27 11:19:59 +1300

    jpsdzfoj

---
+++ Source/GGJC/AIController2.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AIController2.h"
+
+
+
+
---
+++ Source/GGJC/AIController2.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AIController2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJC_API AAIController2 : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
